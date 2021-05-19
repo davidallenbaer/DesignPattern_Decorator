2b2f8e9d93a575a505c4c044f40843690d58fd0b
davidallenbaer <davidallenbaer@gmail.com>
2021-05-19 10:39:12 -0400

    Add project files.

---
+++ DesignPattern_Decorator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DesignPattern_Decorator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ICar bmwCar1 = new BMWCar();
+            bmwCar1.ManufactureCar();
+            Console.WriteLine(bmwCar1 + "\n");
+
+            DieselCarDecorator carWithDieselEngine = new DieselCarDecorator(bmwCar1);
+            carWithDieselEngine.ManufactureCar();
+            Console.WriteLine();
+
+            ICar bmwCar2 = new BMWCar();
+            PetrolCarDecorator carWithPetrolEngine = new PetrolCarDecorator(bmwCar2);
+            carWithPetrolEngine.ManufactureCar();
+            Console.ReadKey();
+        }
+    }
+}
